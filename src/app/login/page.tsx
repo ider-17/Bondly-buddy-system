e683391190616784c2c9876043d4ff801d9d27a1
ider-17 <ider7037@gmail.com>
2025-06-24 21:06:54 +0800

    jkl

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,8 @@
 function Login() {
     return (
-        <div className="w-full h-screen flex justify-center items-center bg-slate-50"></div>
+        <div className="w-full h-screen flex justify-center items-center bg-slate-50">
+            <div></div>
+            <div></div>
+        </div>
     )
 }
