aa371ca0888598ddb4b8c699afd82e03706d628f
24LU4966 24LU4966 <24LU4966@ADMINs-iMac.local>
2025-06-30 15:31:09 +0800

    fixed

--- src/app/buddy/home/page.tsx
+++ src/app/buddy/home/page.tsx
@@ -228,7 +228,7 @@ export default function NewbieHome() {
             <div className="w-1/2 space-y-5">
               <div className='rounded-lg border border-gray-200 py-5 px-6 space-y-5 bg-white'>
                 <div className="space-y-5">
-                  <h6 className='text-lg font-medium'>Шинэ ажилтны прогресс</h6>
+                  <h6 className='text-lg font-bold'>Шинэ ажилтны прогресс</h6>
 
                   <hr />
 
