d7289ecf53f702b74640c0370b8bab7ea91b97a4
ider-17 <ider7037@gmail.com>
2025-07-01 18:29:32 +0800

    pull

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -21,7 +21,7 @@ function Progress({
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-green-600 h-full w-full flex-1 transition-all"
+        className="bg-[#01AA2B] h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
