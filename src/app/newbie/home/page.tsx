b3b98e781f6f0ad7de086e9ceb5ee180aebab4ff
ider-17 <ider7037@gmail.com>
2025-07-01 12:12:16 +0800

    tanii undsen hamtragch done

--- src/app/newbie/home/page.tsx
+++ src/app/newbie/home/page.tsx
@@ -37,6 +37,7 @@ import { toast } from "sonner";
 import MoreInformation from "@/app/_components/MoreInformation";
 import MyInterests from "@/app/_components/MyInterests";
 import CareerGoals from "@/app/_components/CareerGoals";
+import BuddyProfile from "@/app/_components/BuddyProfile";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -386,7 +387,7 @@ export default function NewbieHome() {
             </div>
           </header>
 
-          <div className="flex gap-5 py-10 px-20 bg-slate-100">
+          <div className="flex gap-5 py-10 px-20 bg-slate-100 min-h-screen">
             <div className="w-1/2 space-y-5">
               <RotatingBuddyCard />
 
@@ -689,6 +690,42 @@ export default function NewbieHome() {
               </div>
 
               <CareerGoals />
+            </div>
+          </div>
+        </div>
+      );
+    } else if (selectedSection === "Таны үндсэн хамтрагч") {
+      return (
+        <div>
+          <header className="h-fit header p-5 px-20 flex bg-white items-center border-b border-gray-200">
+            <div>
+              <h6 className="text-base font-medium">Таны үндсэн хамтрагч</h6>
+              <p className="text-xs font-medium text-neutral-500">
+                Таны onboarding үйл явцын туршид тогтмол дэмжин тусалж хамт байх таны зөвлөх ментор
+              </p>
+            </div>
+          </header>
+
+          <div className="bg-slate-100 py-10 px-20 min-h-screen space-y-5">
+            <BuddyProfile />
+            <MyInterests />
+
+            <div className="py-5 px-6 rounded-xl bg-white border border-gray-200 space-y-5">
+              <h5 className="text-lg font-semibold">Танилцуулга</h5>
+              <p className="text-sm">Сайн уу👋. Намайг Тайванбат гэдэг. Би энэ компанид 4 дэх жилдээ ажиллаж байна. Чиний Primary Buddy-гийн хувьд onboarding-ийн бүх хугацаанд тогтмол дэмжлэг үзүүлэх хүн чинь байх болно. 4 жилийн хугацаанд олон төрлийн төсөл дээр ажиллаж, шинэ гишүүдийг амжилттай адапт хийхэд нь тусалж байсан туршлагатай. Би UX/UI дизайны чиглэлээр мэргэшсэн бөгөөд олон салбарын хамтарсан төслүүдийг удирдан ажиллаж байсан туршлагатай. Хамгийн анхны өдрөөс эхлээд бүх үе шатанд чамайг дэмжинэ🤗. Асуух, санал солилцох зүйл байвал хэзээд нээлттэй шүү🙌</p>
+
+              <hr />
+
+              <div className="flex gap-6 justify-center text-center">
+                <div>
+                  <p className="text-lg font-bold text-blue-700">0</p>
+                  <p className="text-sm font-medium">Ажлын туршлага (жилээр)</p>
+                </div>
+                <div>
+                  <p className="text-lg font-bold text-green-700">0</p>
+                  <p className="text-sm font-medium">Тусалсан шинэ ажилтнууд</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
